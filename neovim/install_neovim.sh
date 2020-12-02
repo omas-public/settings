ce61569866a6ef71f99d17e7d507cca73d24863c
Omas Naohiko@IT College Okinawa <omas@std.it-college.ac.jp>
2020-12-02 01:07:26 +0000

    fix install script

--- neovim/install_neovim.sh
+++ neovim/install_neovim.sh
@@ -15,9 +15,10 @@ sudo apt install curl
 curl -fLo "$_PLUGIN_DIR/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo 'select languege'
 echo '1) node 2) python3'
 read input
 case "$input" in
-    '1') exec node.conf.sh;;
-    '2') exec python3.conf.sh;;
+    '1') exec ./node.conf.sh;;
+    '2') exec ./python3.conf.sh;;
 esac
