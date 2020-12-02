adfe7c527abcf28918a50fad5426a809a7db7033
Omas Naohiko@IT College Okinawa <omas@std.it-college.ac.jp>
2020-12-02 01:19:59 +0000

    add linter

--- neovim/node.conf.sh
+++ neovim/node.conf.sh
@@ -3,4 +3,4 @@
 _CONFIG_DIR="$HOME/.config/nvim"
 
 cp ./conf/node/{init.vim,local_bundles.vim,local_init.vim} $_CONFIG_DIR
-sudo npm install -g prettier-standard
+sudo npm install -g prettier-standard standard
